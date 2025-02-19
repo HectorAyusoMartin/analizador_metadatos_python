63b6d39bfbba2b419bc6d3b68e0336073ceaaf05
HectorAyusoMartin <hector.ayuso.martin@gmail.com>
2025-02-19 11:34:40 +0100

    Añadida la función de extracción de metadatos para ficheros PDF, ademas de usar una funcion con una expresion regular , que busca correos electrónicos dentro del PDF y los guarda en una [lista]

--- analizador_metadatos.py
+++ analizador_metadatos.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 from PIL import Image
 import mimetypes
+from pdfminer.high_level import extract_text
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+import re
+import docx
 
 
 class MetadataExtractor(ABC):
@@ -39,7 +44,69 @@ class ImageMetadataExtractor(MetadataExtractor):
                 
             else:
                 return {'Error' : 'Formato de imagen no soportado'}
-             
+    
+class PdfMetadataExtractor(MetadataExtractor):
+    def extract(self, filepath):
+        metadata = {}
+        with open(filepath, 'rb') as f:
+            parser = PDFParser(f)
+            doc = PDFDocument(parser)
+            if doc.info:
+                for info in doc.info:
+                    for key, value in info.items():
+                        #verificamos si el valor de la clave son bytes
+                        if isinstance(value, bytes):
+                            try:
+                                #Intentaar decodificar en UTF-16BE
+                                decoded_value = value.decode('utf-16be')
+                            except UnicodeDecodeError:
+                                #Intentar decodificarlo en UTF-8
+                                decoded_value =  value.decode('utf-8', errors='ignore')
+                          
+                        else:
+                            decoded_value = value
+                            
+                        metadata[key] = decoded_value
+            #Procesamos el texto del PDF para obtener otros datos de interes
+            text = extract_text(filepath)
+            metadata['Emails'] = self._extract_emails(text)
+        return metadata
+    
+                    
+                                 
+    def _extract_emails(self, text):
+        """Va a utilizar una expresión regular, encontrada en internet, para enccontrar emails en el texto"""
+        email_regex = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
+        return re.findall(email_regex, text)        
+
+class DocxMetadataExtractor(MetadataExtractor):
+    
+    def extract(self, filepath):
+        doc = docx.Document(filepath)
+        prop = doc.core_properties
+        attributes = [
+            
+            "author",   # Autor del documento
+            "category", # Categoría del documento 
+            "comments", # Comentarios del documento
+            "content_status", # Estado del contenido
+            "created",  # Fecha de creación
+            "identifier", # Identificador
+            "keywords", # Palabras clave
+            "last_modified_by", # Última persona que modificó el documento
+            "last_printed", # Última fecha de impresión
+            "language", # Idioma
+            "modified", # Fecha de modificación
+            "subject", # Asunto
+            "title", # Título
+            "version" # Versión
+        ]
+        
+        metadata = {attr: getattr(prop, attr, None) for attr in attributes}
+        
+        
+    
+     
 class MetadataExtractorFactory:
     @staticmethod
    
@@ -49,6 +116,8 @@ class MetadataExtractorFactory:
         if tipo_de_archivo:
             if tipo_de_archivo.startswith('image'):
                 return ImageMetadataExtractor()
+            if tipo_de_archivo == 'application/pdf':
+                return PdfMetadataExtractor()
             
     @staticmethod      
     def extract_metadata(filepath):
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from analizador_metadatos import extract_metadata
 if __name__ == '__main__':
     
     #filepath = r"C:\Users\Héctor\OneDrive\Escritorio\Python en Ciberseguridad Udemy\Analisis de metadatos con Python\analizador_metadatos\Descargas\ejemplo_imagen_metadatos_gps.jpg"
-    filepath = 'analizador_metadatos\imagen_prueba.jpg'
+    filepath = r'analizador_metadatos\imagen_prueba.jpg'
+    filepath = r'analizador_metadatos\PDF_metadata.pdf'
     metadata = extract_metadata(filepath)
     
     for clave, valor in metadata.items():
